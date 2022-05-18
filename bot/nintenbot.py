e9c24ae6ca2af2d833f85b6e324aa1b2ce0deff2
Bishal Khanal <bishal.khanal87@gmail.com>
2022-05-18 10:22:18 -0700

    welcome message and controls

--- bot/nintenbot.py
+++ bot/nintenbot.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 import keyboard
 import os
 
+
 load_dotenv()
 
 class Playbot(commands.Bot):
+
+
+  blacklist= ['shit', 'fuck', 'damn', 'etc']
 
   def __init__(self):
     self.twitchplayactive = False
@@ -36,6 +40,49 @@ class Playbot(commands.Bot):
   @commands.command()
   async def start(self, ctx: commands.Context):
     keyboard.press_and_release('return')
+
+  @commands.command()
+  async def hello(self, ctx: commands.Context):
+    await ctx.send(f'Hello {ctx.author.name}!')
+
+  @commands.command()
+  async def controls(self, ctx: commands.Context):
+        # shouts out the controls
+    await ctx.send(f'Hello {ctx.author.name}! controls: !up, !down, !left, !right. !a, !b and !start')
+
+  #Explicit Chat Filter ON/Off    
+  @commands.command(name="explicitfilter")
+    # Check to see if is moderator to activate
+  async def explicit_filter(self, ctx: commands.Context):
+        if ctx.author.is_mod or ctx.author.is_broadcaster:
+            if self.explicitfilter is False:
+                self.explicitfilter = True
+                await ctx.send('Kiddie filter on!')
+            if self.explicitfilter is True:
+                self.explicitfilter = False
+                await ctx.send('Kiddie filter off!')
+   
+    # Check chat for blacklisted words
+  async def event_message(self, ctx: commands.Context):
+        if ctx.message.echo:
+            return
+        if self.explicitfilter is False:
+            return
+        if self.explicitfilter is True:
+            if ctx.author.is_broadcaster:
+                return
+            if any([word in ctx.message.content for word in self.blacklist]):
+                self.timeout(ctx = commands.Context, user = ctx.author.name,  duration = 300, reason ='You said a bad word')
+
+  @commands.command()
+  async def addblacklist(self, ctx : commands.context, message):
+        self.blacklist.append(message)
+        await ctx.send(f'{message} has been added to blacklist there are {len(self.blacklist)} words banned from chat')
+
+  @commands.command()
+  async def timeout(self, ctx: commands.Context, user, duration : int = 300, *, reason : str = ''):
+        await ctx.send(f'/timeout {user} {duration} {reason}')
+    
 
 bot = Playbot()
 bot.run()
